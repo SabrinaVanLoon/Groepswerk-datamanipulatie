a7dadf59dbc1d1f93f350ff40a98692089e8e663
Steen <61277078+r0703237@users.noreply.github.com>
2020-04-30 14:39:53 +0200

    Add files via upload

---
+++ FarmingSimulator/FarmingSimulator_WPF/DierenWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FarmingSimulator_WPF
+{
+    /// <summary>
+    /// Interaction logic for DierenWindow.xaml
+    /// </summary>
+    public partial class DierenWindow : Window
+    {
+        public DierenWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
