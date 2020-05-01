84e958e981fea8d9df0efb946e0528835e4b1f3d
DorienVanLooy <nuana2009@gmail.com>
2020-05-01 08:42:09 +0200

    toevoegen scherm opdrachten

---
+++ FarmingSimulator/FarmingSimulator_WPF/Opdrachten.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FarmingSimulator_WPF
+{
+    /// <summary>
+    /// Interaction logic for Opdrachten.xaml
+    /// </summary>
+    public partial class Opdrachten : Window
+    {
+        public Opdrachten()
+        {
+            InitializeComponent();
+        }
+
+        private void btnZoekVoertuigOpType_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnZoekVoertuigOpNaam_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnKopen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnHuren_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnTerugNaarMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
