0771fb6a7d06c60235741f34c2721652e3889ae3
Stijn Weyts <r0703237@student.thomasmore.be>
2020-05-11 13:29:35 +0200

    toevoegen code spelers

--- FarmingSimulator/FarmingSimulator_WPF/MainWindow.xaml.cs
+++ FarmingSimulator/FarmingSimulator_WPF/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using EntityFramework_DAL;
+using FarmingSimulator_MODELS;
 
 namespace FarmingSimulator_WPF
 {
@@ -25,10 +27,52 @@ namespace FarmingSimulator_WPF
             InitializeComponent();
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            cmbSpelers.ItemsSource = DatabaseOperations.OphalenSpelers();
+        }
+
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
             Menu menu = new Menu();
             menu.ShowDialog();
+        }
+
+        private void btnNieuweSpeler_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtNieuweSpeler.Text))
+            {
+                a_Speler speler = new a_Speler();
+                speler.naam = txtNieuweSpeler.Text;
+                MessageBoxResult antwoord = MessageBox.Show($"Deze speler toevoegen? {Environment.NewLine} {speler.naam}", "Speler toevoegen", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (antwoord == MessageBoxResult.Yes)
+                {
+                    speler.level = 0;
+                    speler.kapitaal = 10000;
+
+                    MessageBoxResult Geslacht = MessageBox.Show("Is de speler een man?", "", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                    if (Geslacht == MessageBoxResult.Yes)
+                    {
+                        speler.geslacht = "man";
+                    }
+                    else
+                    {
+                        speler.geslacht = "vrouw";
+                    }
+
+                    DatabaseOperations.ToevoegenSpeler(speler);
+                    cmbSpelers.Items.Refresh();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Gelieve een naam voor de nieuwe speler in te geven.", "Foutmelding", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnAfsluiten_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
         }
     }
 }
