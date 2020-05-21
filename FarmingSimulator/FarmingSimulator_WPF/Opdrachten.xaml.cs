a0d2e2a3253a69df3e91ad4822e8c49139e110d8
DorienVanLooy <nuana2009@gmail.com>
2020-05-21 08:42:53 +0200

    verwijderen van voorwaarden

--- FarmingSimulator/FarmingSimulator_WPF/Opdrachten.xaml.cs
+++ FarmingSimulator/FarmingSimulator_WPF/Opdrachten.xaml.cs
@@ -40,7 +40,7 @@ namespace FarmingSimulator_WPF
 
                 Voorwaarden.taakomschrijving = opdracht.taakomschrijving;
                 Voorwaarden.beloning = opdracht.beloning;
-                Voorwaarden.eigenaar_Id = opdracht.eigenaar_Id;
+              //  Voorwaarden.eigenaar_Id = opdracht.eigenaar_Id;
                 Voorwaarden.graansoort_Id = opdracht.graansoort_Id;
                 Voorwaarden.veld_Id = opdracht.veld_Id;
 
